5f7fa38102f6fae6dcf2660bec5dbf96d13155f0
Samuel Resendez <samuelresendez@cml07.engr.ucdavis.edu>
2015-08-18 11:48:39 -0700

    Communicates with Firebase somewhat, but its really sketchy

--- kivyLiveChatTest.py
+++ kivyLiveChatTest.py
@@ -8,22 +8,24 @@ from kivy.uix.button import Button
 import requests
 from firebase import firebase
 
-from kivy.adapters.listadapter import 
+from kivy.uix.listview import ListView
+from kivy.adapters.simplelistadapter import SimpleListAdapter
 
 
 
 
 
 class testLiveChatApp(App):
+	data = ["Welcome to X's experimental live chat App!"]
 	inputText = "Please enter your message!"
 	def build(self):
+		self.getData(self,0)
 		root = self.setupUI()
 		return root
 
 
 	def inputTextChanged(self,instance,value):
 		self.inputText = value
-		print(self.inputText)
 
 
 	def getData(self,instance,value):
@@ -31,9 +33,8 @@ class testLiveChatApp(App):
 		data = SamChungsFirebase.get('/', None)
 		for key in data:
 			message = data[key]
-			new_Label = Label(text=message)
-			self.root.remove_widget()
-			self.root.add_widget(new_Label)
+			self.data.append(message)
+
 
 
 
@@ -44,6 +45,7 @@ class testLiveChatApp(App):
 		message = self.inputText
 		SamChungsFirebase.post('/', message)
 		self.getData(self,instance)
+		self.root = self.setupUI()
 
 	def setupUI(self):
 		layout = BoxLayout(orientation = 'vertical')
@@ -57,15 +59,32 @@ class testLiveChatApp(App):
 		
 		inputLayout.add_widget(textInput)
 		inputLayout.add_widget(sendButton)
-
-		title = Label(text = "Welcome to X's experimental live chat App!")
-		layout.add_widget(title)
+		chatListView = ListView(item_strings=self.data)
+		layout.add_widget(chatListView)
 		layout.add_widget(inputLayout)
 
 		return layout
 
 
+	def updateUI(self):
+		self.root.clear_widgets()
+
+		layout = BoxLayout(orientation = 'vertical')
+		inputLayout = BoxLayout(orientation = 'horizontal', size_hint_y=(.05))
+		sendButton = Button(text = 'Enter', size_hint_x = (.2))
+		sendButton.bind(on_press=self.enterChat)
+		textInput = TextInput(text = 'Please enter your message!', multiline = False)
+		textInput.bind(text=self.inputTextChanged)
 	
+
+		
+		inputLayout.add_widget(textInput)
+		inputLayout.add_widget(sendButton)
+		chatListView = ListView(item_strings=self.data)
+		layout.add_widget(chatListView)
+		layout.add_widget(inputLayout)
+
+		return layout
 
 
 
